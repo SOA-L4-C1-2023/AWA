2dc4a9aae279746a75593cb6b8ec22f024566d5e
Uranga Esteban <euadenez8@gmail.com>
2023-06-25 20:51:38 -0300

    Eliminar vistas innecesarias y reemplaza el recyclerview por el intent a bluetooth settings, ademas reconfigura la pantalla de bluetooth deshabilitado para que sea un Dialog

--- Android/app/src/main/java/com/example/androidapp/activity/MainActivity.java
+++ Android/app/src/main/java/com/example/androidapp/activity/MainActivity.java
@@ -3,18 +3,11 @@ package com.example.androidapp.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.Manifest;
@@ -24,8 +17,6 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.androidapp.R;
-import com.example.androidapp.adapter.DeviceRVA;
-import com.example.androidapp.model.Device;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private BluetoothAdapter mBluetoothAdapter;
 
     public static final int MULTIPLE_PERMISSIONS = 10;
+    private AlertDialog enableBluetoothDialog;
 
     //se crea un array de String con los permisos a solicitar en tiempo de ejecucion
     //Esto se debe realizar a partir de Android 6.0, ya que con verdiones anteriores
@@ -42,99 +34,101 @@ public class MainActivity extends AppCompatActivity {
     String[] permissions = new String[]
     {
         Manifest.permission.BLUETOOTH,
-        Manifest.permission.BLUETOOTH_ADMIN,
-        Manifest.permission.BLUETOOTH_SCAN,
-        Manifest.permission.BLUETOOTH_CONNECT,
-        Manifest.permission.ACCESS_COARSE_LOCATION
+        Manifest.permission.BLUETOOTH_ADMIN
     };
-    private Button btnToggleBluetooth;
-    private Button btnScan;
-    private RecyclerView rvDevices;
-    List<Device> devices = new ArrayList<>();
+
+    private Button btnTogglePump;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showToast("onCreate");
+        checkBleutooth();
+    }
 
-        rvDevices = (RecyclerView) findViewById(R.id.rvDevices);
-        btnToggleBluetooth = (Button) findViewById(R.id.btnToggleBluetooth);
-        btnScan = (Button) findViewById(R.id.btnScan);
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (checkPermissions()) {
-            enableComponent();
+    @Override
+    protected void onResume() {
+        showToast("onResume");
+        checkBleutooth();
+        super.onResume();
+    }
 
-            DeviceRVA adapter = new DeviceRVA(devices, getApplication());
-            rvDevices.setAdapter(adapter);
-            rvDevices.setLayoutManager(new LinearLayoutManager(this));
-            RecyclerView.ItemAnimator itemAnimator = new DefaultItemAnimator();
-            itemAnimator.setAddDuration(500);
-            itemAnimator.setRemoveDuration(500);
-            rvDevices.setItemAnimator(itemAnimator);
-        }
+    @Override
+    protected void onStart() {
+        showToast("onStart");
+        checkBleutooth();
+        super.onStart();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        showToast("onWindowFocusChanged");
+        checkBleutooth();
+        super.onWindowFocusChanged(hasFocus);
     }
 
     @Override
     protected void onDestroy() {
-        unregisterReceiver(mReceiver);
         super.onDestroy();
     }
 
-    protected void enableComponent() {
-        //Primero veo si soporta BT
-        if (mBluetoothAdapter == null) {
-            showUnsupported();
-        } else {
-            IntentFilter filter = new IntentFilter();
-            filter.addAction(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-            filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
-            filter.addAction(BluetoothDevice.ACTION_FOUND);
-            filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
-            filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-            registerReceiver(mReceiver, filter);
-            checkEnabled();
+    @SuppressLint("MissingPermission")
+    private void checkBleutooth() {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (checkPermissions()) {
+            if (mBluetoothAdapter == null) {
+                showUnsupported();
+            } else {
+                if (mBluetoothAdapter.isEnabled()/* && mBluetoothAdapter.getName().startsWith("AWA_")*/) {
+                    showToast("Bluetooth activado");
+                    deviceConected();
+                } else {
+                    showToast("Bluetooth desactivado");
+                    showNotConnectedOrNotEnabled();
+                }
+            }
         }
+    }
+
+    private void deviceConected() {
+        btnTogglePump = findViewById(R.id.btnTogglePump);
+        btnTogglePump.setOnClickListener(togglePumpListener);
+        if (enableBluetoothDialog != null && enableBluetoothDialog.isShowing()) {
+            enableBluetoothDialog.dismiss();
+        }
+    }
+
+    private void showNotConnectedOrNotEnabled() {
+        if (enableBluetoothDialog != null && enableBluetoothDialog.isShowing()) {
+            return;
+        }
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View noBluetoothView = getLayoutInflater().inflate(R.layout.activity_bluetooth, null);
+        builder.setView(noBluetoothView);
+
+        Button btnActivateBluetooth = noBluetoothView.findViewById(R.id.btnActivarBt);
+        btnActivateBluetooth.setOnClickListener(activarBluetoothRequestListener);
+
+        Button btnCloseApp = noBluetoothView.findViewById(R.id.btnCloseApp);
+        btnCloseApp.setOnClickListener(cerrarAppListener);
+
+        enableBluetoothDialog = builder.create();
+        enableBluetoothDialog.show();
     }
 
     private void showUnsupported() {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        View dialogView = getLayoutInflater().inflate(R.layout.dialog_no_bluetooth, null);
-        builder.setView(dialogView);
+        View noBluetoothView = getLayoutInflater().inflate(R.layout.dialog_no_bluetooth, null);
+        builder.setView(noBluetoothView);
 
-        Button btnClose = dialogView.findViewById(R.id.btnClose);
-        btnClose.setOnClickListener(v -> {
-            finish(); // Cierra la aplicación
-        });
+        Button btnClose = noBluetoothView.findViewById(R.id.btnClose);
+        btnClose.setOnClickListener(cerrarAppListener);
 
         AlertDialog dialog = builder.create();
         dialog.show();
-    }
 
-    private void showDisabled() {
-        btnScan.setEnabled(false);
-        btnScan.setVisibility(View.INVISIBLE);
-        rvDevices.setEnabled(false);
-        rvDevices.setVisibility(View.INVISIBLE);
-        btnToggleBluetooth.setText(R.string.activar_bt);
-    }
-
-    private void showEnabled() {
-        btnScan.setEnabled(true);
-        btnScan.setVisibility(View.VISIBLE);
-        rvDevices.setEnabled(true);
-        rvDevices.setVisibility(View.VISIBLE);
-        btnToggleBluetooth.setText(R.string.desactivar_bt);
-    }
-
-    private void checkEnabled() {
-        if (mBluetoothAdapter.isEnabled()) {
-            showEnabled();
-            btnToggleBluetooth.setOnClickListener(btnToggleBTListener);
-            btnScan.setOnClickListener(btnScanListener);
-        } else {
-            showDisabled();
-        }
     }
 
     private boolean checkPermissions() {
@@ -156,69 +150,32 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        @SuppressLint("MissingPermission")
-        public void onReceive(Context context, Intent intent) {
-            showToast("BroadcastReceiver");
-            //Atraves del Intent obtengo el evento de Bluethoot que informo el broadcast del SO
-            String action = intent.getAction();
-
-            //Si cambio de estado el Bluethoot(Activado/desactivado)
-            if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)) {
-                //Obtengo el parametro, aplicando un Bundle, que me indica el estado del Bluetooth
-                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
-                checkEnabled();
-                //Si esta activado
-                if (state == BluetoothAdapter.STATE_ON) {
-                    showEnabled();
-                    showToast("Activado");
-                } else if (state == BluetoothAdapter.STATE_OFF) {
-                    showDisabled();
-                    showToast("Desactivado");
-                }
-            }
-            //Si se inicio la busqueda de dispositivos bluethoot
-            else if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
-                btnScan.setEnabled(false);
-                showToast("Iniciando Busqueda");
-            }
-            //Si finalizo la busqueda de dispositivos bluethoot
-            else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-                btnScan.setEnabled(true);
-                showToast("Busqueda Finalizada");
-            }
-            //si se encontro un dispositivo bluethoot
-            else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                BluetoothDevice device = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                devices.add(0, new Device(device.getName(), device.getAddress(), device));
-                rvDevices.getAdapter().notifyDataSetChanged();
-                showToast("Dispositivo Encontrado:" + device.getName());
-            }
-        }
-    };
-
     private void showToast(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
-    @SuppressLint("MissingPermission")
-    public void toggleBluetooth() {
-        Intent intent = new Intent(Settings.ACTION_BLUETOOTH_SETTINGS);
-        startActivity(intent);
-    }
 
-    private final View.OnClickListener btnToggleBTListener = new View.OnClickListener() {
+    private final View.OnClickListener activarBluetoothRequestListener = new View.OnClickListener() {
         @SuppressLint("MissingPermission")
         @Override
         public void onClick(View v) {
-            toggleBluetooth();
+            Intent intent = new Intent(Settings.ACTION_BLUETOOTH_SETTINGS);
+            startActivity(intent);
         }
     };
 
-    private final View.OnClickListener btnScanListener = new View.OnClickListener() {
+    private final View.OnClickListener cerrarAppListener = new View.OnClickListener() {
         @SuppressLint("MissingPermission")
         @Override
         public void onClick(View v) {
-            toggleBluetooth();
+            finish();
+        }
+    };
+
+    private final View.OnClickListener togglePumpListener = new View.OnClickListener() {
+        @SuppressLint("MissingPermission")
+        @Override
+        public void onClick(View v) {
+            showToast("togglePumpListener");
         }
     };
 
