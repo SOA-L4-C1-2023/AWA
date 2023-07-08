139ec43801d38820ed942b4f80739625bdc5ca2d
webber0 <42701029+webber0@users.noreply.github.com>
2023-07-08 18:10:49 -0300

    Limpieza de código
    Se limpia el código sacando mensajes de debug y lineas comentadas

--- Arduino/Codigo/awa.ino
+++ Arduino/Codigo/awa.ino
@@ -194,7 +194,6 @@ double actual_water_distance;
 void setup()
 {
   initialize_sistem();
-  DebugPrintEstado(states_s[current_state], events_s[new_event]);
 }
 
 void loop()
@@ -212,14 +211,14 @@ void state_machine()
   {
     if( new_event != EV_CONT )
     {
-      DebugPrintEstado(states_s[current_state], events_s[new_event]);
+	    
     }
     
     state_table_actions[current_state][new_event]();
   }
   else
   {
-    DebugPrintEstado(states_s[ST_SUSPENDED_LOAD], events_s[EV_UNKNOWN]);
+	  
   }
   // Consumo el evento...
   new_event   = EV_CONT;
@@ -248,9 +247,6 @@ void initialize_sistem()
   pinMode(RELAY_PIN       , OUTPUT); 
   pinMode(SOLENOIDE,OUTPUT);
   attachInterrupt(digitalPinToInterrupt(FLOWMETER_PIN), ISRCountPulse, RISING);
-
-  //servo_1.attach(SERVO_1_PIN);
-  //servo_1.write(WATER_GATE_CLOSE_ANGLE);
   
   timeout = false;
   past_time = millis();
@@ -265,12 +261,8 @@ void initialize_sistem()
 
 void get_new_event()
 {
-  DebugPrint("Get new event");
   actual_water_distance = TANK_DEPTH - get_distance_to_the_water();
   double actual_water_flow = get_water_flow();
-  //Serial.print("WATERFLOW:\n");
-  //Serial.print(actual_water_flow);
-  //Serial.print("--------\n");
   long current_time = millis();
 
   check_bluetooth();
@@ -307,8 +299,6 @@ void get_new_event()
       } 
     } else 
     {
-      Serial.print("-----------------");
-      Serial.print(actual_water_flow);
       new_event = EV_PRESSURE;
     }
   }
@@ -328,16 +318,10 @@ float get_water_flow()
     pulseConter = 0;
     interrupts(); // vuelvo a capturar las interrupciones para contar nuevamente.
     lastRefreshTime = millis();
-    //Serial.print("Nuevo Flujo: ");
-    //Serial.print(lastFlow);
-    //Serial.println("L/min");
   }
   else{
     if( millis() - lastRefreshTime >= FM_REFRESH_RATE/3)
     {
-      //Serial.print("Flujo Anterior: ");
-      //Serial.print(lastFlow);
-      //Serial.println("L/min");
     }
   }
   return lastFlow;
@@ -375,7 +359,6 @@ void none();
 //-----------------------------------------------------
 void show_level()
 {
-  DebugPrint("Show level");
   double actual_water_distance = get_distance_to_the_water();
   
   if (actual_water_distance < LOW_WATER_LEVEL)
@@ -394,13 +377,11 @@ void show_level()
 void open_water_gate()
 {
   digitalWrite(SOLENOIDE,HIGH);
-  //servo_1.write(WATER_GATE_OPENING_ANGLE);
   
 }
 void close_water_gate()
 {
   digitalWrite(SOLENOIDE,LOW);
-  //servo_1.write(WATER_GATE_CLOSE_ANGLE);
   
 }
 
@@ -418,14 +399,11 @@ void stop_water_pump()
 
 void error()
 {
-  DebugPrintEstado(states_s[current_state], events_s[new_event]);
-  DebugPrint("Something blew up!");
   if (last_state == ST_PRESSURIZED_LOAD) 
   {
     stop_water_pump();
   }
   drop_service_test_timer();
- // close_water_gate();
   turn_on_white_led();
   current_state = ST_GATE_CLOSED;
 }
@@ -437,7 +415,6 @@ void none()
 //-----------------------------------------------------
 void start_load()
 {
-  DebugPrint("Start Load");
   if (service_timer != NULL && service_timer != 0) 
   {
     drop_service_test_timer();
@@ -449,7 +426,6 @@ void start_load()
 
 void finish_load()
 {
-  DebugPrint("Finish load");
   // Apago la bomba
   if (last_state == ST_PRESSURIZED_LOAD)
   {
@@ -467,7 +443,6 @@ void finish_load()
 //-----------------------------------------------------
 void start_presurized_load()
 {
-  DebugPrint("Start presurized load");
   digitalWrite(SOLENOIDE,HIGH);
   start_water_pump();
   setup_flow_timer();
@@ -478,8 +453,6 @@ void start_presurized_load()
 
 void start_pump_cooldown()
 {
-  DebugPrint("Start water pump cooldown");
-  //open_water_gate();
   stop_water_pump();
   setup_flow_timer();
   setup_pump_timer();
@@ -489,8 +462,6 @@ void start_pump_cooldown()
 
 void handle_hot_pump()
 {
-  DebugPrint("Handle hot pump");
-  //open_water_gate();
   setup_flow_timer();
   turn_on_blue_led();
   current_state = ST_SUSPENDED_LOAD;
@@ -498,8 +469,6 @@ void handle_hot_pump()
 
 void handle_service_down()
 {
-  DebugPrint("Handle service down");
-  //open_water_gate();
   stop_water_pump();
   drop_pump_timer();
   setup_flow_timer();
@@ -510,7 +479,6 @@ void handle_service_down()
 
 void test_service()
 {
-  DebugPrint("Handle test servie");
   start_water_pump();
   drop_service_test_timer();
   setup_flow_timer();
@@ -521,54 +489,43 @@ void test_service()
 
 void drop_pump_timer()
 {
-  DebugPrint("Drop pump timer");
   pump_timer = NULL;
 }
 
 void drop_service_test_timer() 
 {
-  DebugPrint("Drop service test timer");
   service_timer = NULL;
 }
 
 void setup_service_test_timer() 
-{
-  DebugPrint("Setup servcice test timer");
+{ 
   service_timer = millis();
 }
 
 void setup_pump_timer()
 {
-  DebugPrint("Set up pump timer");
   pump_timer = millis();
 }
 
 void drop_flow_timer() 
 {
-  DebugPrint("Drop flow timer");
-  //open_water_gate();
   flow_timer = NULL;
 }
 
 void setup_flow_timer() 
 {
-  DebugPrint("Setup flow timer");
   flow_timer = millis();
 }
 
 void drop_test_timer() 
 {
   int flow = get_water_flow();
-  DebugPrint("Drop test timer");
-  // open_water_gate();
   if (flow > MIN_FLOW_EXPECTED)
   {
-    DebugPrint("Test passed, start presurized load");
     start_presurized_load();
   }
   else
   {
-    DebugPrint("Test failed, handle service down");
     handle_service_down();
   }
   test_timer = NULL;
@@ -576,7 +533,6 @@ void drop_test_timer()
 
 void setup_test_timer() 
 {
-  DebugPrint("Setup test timer");
   test_timer = millis();
 }
 
